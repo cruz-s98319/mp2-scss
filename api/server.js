aa80627d4d39a38d12c978c92a5816369b8d727d
Samantha Cruz <cruz.s98319@gmail.com>
2025-01-18 19:02:18 -0800

    Fixed fetch issue by removing .env from .gitignore file. Redefined MONGO_URI connection and frontend server origin for CORS.

--- api/server.js
+++ api/server.js
@@ -1,32 +1,34 @@
-const express = require('express'); 
-const cors = require('cors'); 
-const mongoose = require('mongoose'); 
-require('dotenv').config(); 
-
-//Execute express 
+const express = require('express');
 const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const Todo = require('./models/Todo');
+const { todo } = require('node:test');
 
-const port = 4001; 
+require('dotenv').config({path: '../.env'})
+console.log('MONGO_URI:', process.env.MONGO_URI);
 
-//Middlewares
-app.use(express.json()); 
-app.use(cors());
+const port = 4001;
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3000'
+  }));
 
-const connectionString = process.env.MONGO_URI;
-mongoose.connect(connectionString)
-        .then(() => console.log('Connected to the database…')) 
-        .catch((err) => console.error('Connection error:', err));
 
-//Routes 
-const Todo = require(‘./models/Todo’);
+const connectionString = process.env.MONGO_URI
 
-app.get('/todo', async (req, res) => { 
-    const todos = await Todo.find(); 
-    res.json(todos); });
+mongoose.connect(connectionString).then(()=> console.log('Connect to the DB..')).catch((err)=>console.log(err))
 
-app.post('/todo/new', async (req,res) => {
-    const newTask = await Todo.create(req.body);
-    res.status(201).json({newTask})
+
+//routes 
+app.get('/todo', async(req,res)=> {
+    const todos = await Todo.find();
+    res.json(todos)
+})
+
+app.post('/todo/new', async(req,res)=> {
+    const task = await Todo.create(req.body)
+    res.status(201).json({task})
 })
 
 app.delete('/todo/delete/:id', async(req,res)=>{
@@ -34,4 +36,5 @@ app.delete('/todo/delete/:id', async(req,res)=>{
     res.json(result)
 })
 
-app.listen(port, () => console.log(`Server is running on port ${port}`)); 
+
+app.listen(port, console.log(`server is running on port ${port}`))
--- todo/src/App.js
+++ todo/src/App.js
@@ -1,7 +1,45 @@
 import { useEffect, useState } from "react";
 import TodoItem from "./TodoItem";
 
+const API_BASE= 'http://localhost:4001/todo';
+
 function App() {
+
+  const [items, setItems] = useState([]);
+  const [input, setInput] = useState("");
+
+
+  useEffect(() => {
+    GetTodos();
+  }, []);
+
+  const handleChange = (e) => {
+    setInput(e.target.value)
+  }
+
+  const GetTodos = () => {
+    fetch(API_BASE)
+    .then(res => res.json())
+    .then(data => setItems(data))
+    .catch(err => console.log(err))
+  }
+
+  const addItem = async() => {
+   const data = await fetch(API_BASE + "/new", {
+    method: "POST",
+    headers: {
+      "content-type" : "application/json"
+    },
+    body: JSON.stringify({
+      name: input,
+      completed: false
+    })
+   }).then(res => res.json()) 
+   console.log(data)
+   await GetTodos()
+   setInput('')
+  }
+
 
   return (
     <div className="container">
@@ -10,17 +48,22 @@ function App() {
       </div>
 
       <div className="form">
-        <input type='text'></input>
-        <button>
+        <input type='text' value={input} onChange={handleChange}></input>
+        <button onClick={()=>addItem()}>
           <span>ADD</span>
         </button>
       </div>
 
       <div className="todolist">  
-        <TodoItem/>  
+      {items.map((item)=> {
+        const {_id, name, completed} = item
+        return  <TodoItem name={name} id={_id} completed={completed} setItems={setItems}/>   
+      })}
+     
       </div>
     </div>
- 
+  
+   
   );
 }
 
